7c7cbb94b12afcfa84daeb998e31efce9fa2adc1
Albert O'Connor <amjoconn@gmail.com>
2018-01-10 10:25:54 -0500

    Bump to version 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='django-raise',
-    version='0.0.2',
+    version='0.0.3',
     description='Raise money from pledges',
     long_description=long_description,
     url='https://github.com/AvocadoCoop/django-raise',
