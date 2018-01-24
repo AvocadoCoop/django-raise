be083e00f8f8c58201a92e1ac363e73fbe1f6f4e
Albert O'Connor <amjoconn@gmail.com>
2018-01-24 13:56:00 -0500

    Add default messages, bump to 0.0.5

--- raiseapp/views.py
+++ raiseapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib import messages
 
 from . import models
 from . import forms
@@ -27,8 +28,12 @@ def pledge(request, slug, post_pledge=None, template="pledge.html"):
             pledge.save()
 
             if post_pledge:
-                post_pledge(pledge)
+                post_pledge(request, pledge)
 
+            messages.info(
+                request,
+                'Thanks for your pledge! You will receive an email shortly with more details.'
+            )
             return redirect('campaign', campaign.slug)
     else:
         form = forms.PledgeForm()
@@ -53,8 +58,12 @@ def reminder(request, slug, post_reminder=None, template="reminder.html"):
             reminder.campaign = campaign
             reminder.save()
             if post_reminder:
-                post_reminder(reminder)
+                post_reminder(request, reminder)
 
+            messages.info(
+                request,
+                'We will email you a reminder a few days before the campaign ends!'
+            )
             return redirect('campaign', campaign.slug)
 
     else:
