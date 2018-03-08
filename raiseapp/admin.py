5280017547b84c8879ebbd1a588bb9e5204b488d
Albert O'Connor <amjoconn@gmail.com>
2018-03-08 09:41:41 -0500

    Add admin support, bump version.

--- raiseapp/admin.py
+++ raiseapp/admin.py
@@ -3,9 +3,16 @@ from django.contrib import admin
 from . import models
 
 
+class PledgeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'active', 'amount', 'created')
+    list_filter = ('campaign', 'active')
+
+
 admin.site.register([
     models.Campaign,
     models.Reward,
     models.Reminder,
-    models.Pledge,
 ])
+
+
+admin.site.register(models.Pledge, PledgeAdmin)
